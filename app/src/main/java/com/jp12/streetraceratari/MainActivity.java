f273da95b282a2c875d1eafc2924ea091ec1ccf5
joshpatra12@gmail.com <joshpatra12@gmail.com>
2022-05-15 12:28:55 -0400

    add tapping to inc speed, add enemy car, add lanes, add movement

--- app/src/main/java/com/jp12/streetraceratari/MainActivity.java
+++ app/src/main/java/com/jp12/streetraceratari/MainActivity.java
@@ -17,6 +17,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -37,6 +38,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     SensorManager mSensorManager;
     Sensor mAccelerometer;
     int carPos = 220;
+    int enemyCarPos = 0;
+    int changeY = 2;
+    int enemyCarX = 500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,18 +68,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
         float x = (int) event.values[0];
+        if(enemyCarPos >= 1800){
+            enemyCarPos = 000;
+            int rando = (int)(Math.random()*8+1);
+            if(rando == 1) enemyCarX = 200;
+            else if (rando == 2) enemyCarX = 300;
+            else if (rando == 3) enemyCarX = 400;
+            else if (rando == 4) enemyCarX = 500;
+            else if (rando == 5) enemyCarX = 600;
+            else if (rando == 6) enemyCarX = 700;
+            else if (rando == 7) enemyCarX = 800;
+            else if (rando == 8) enemyCarX = 900;
+            System.out.println(rando);
+        }
+        enemyCarPos += changeY;
+        System.out.println(enemyCarPos);
         if(carPos > 900) carPos = 900;
         if(carPos < 195) carPos = 195;
         if(x >= 7 && carPos - 20 >= 195) {carPos -= 20; return;}
         if(x >= 5 && carPos - 10 >= 195) {carPos -= 10; return;}
         if(x >= 3 && carPos - 8 >= 195) {carPos -= 8; return;}
         if(x >= 2 && carPos - 5 >= 195) {carPos -= 5; return;}
-        if(x >= 1 && carPos - 3 >= 195) {carPos -= 3;} else if (x >= 1 && carPos + 3 <= 165){ carPos -= 1; return;}
+        if(x >= 0.5 && carPos - 3 >= 195) {carPos -= 3;} else if (x >= 0.5 && carPos - 3 <= 195){ carPos -= 1; return;}
         if(x <= -7 && carPos + 20 <= 900) {carPos += 20; return;}
         if(x <= -5 && carPos + 10 <= 900) {carPos += 10; return;}
         if(x <= -3 && carPos + 8 <= 900) {carPos += 8; return;}
         if(x <= -2 && carPos + 5 <= 900) {carPos += 5; return;}
-        if(x <= -1 && carPos + 3 <= 900) {carPos += 3;} else if (x <= -1 && carPos + 3 >= 900){ carPos += 1; return;}
+        if(x <= -0.5 && carPos + 3 <= 900) {carPos += 3;} else if (x <= -0.5 && carPos + 3 >= 900){ carPos += 1; return;}
     }
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -90,6 +109,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         SurfaceHolder holder;
         volatile boolean running = false;
         Bitmap car;
+        Bitmap enemyCar;
         Bitmap bg;
         Paint paintProperty;
 
@@ -101,6 +121,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             holder = getHolder();
 
             car = BitmapFactory.decodeResource(getResources(), R.drawable.car);
+            enemyCar = BitmapFactory.decodeResource(getResources(), R.drawable.enemycar);
             bg = BitmapFactory.decodeResource(getResources(), R.drawable.atari);
             Display screenDisplay = getWindowManager().getDefaultDisplay();
             Point sizeOfScreen = new Point();
@@ -114,17 +135,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
 
         @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            if(changeY == 2) {changeY = 4;}
+            else changeY = 2;
+            return super.onTouchEvent(event);
+        }
+
+        @Override
         public void run() {
             while (running) {
                 if (!holder.getSurface().isValid())
                     continue;
                 Canvas canvas = holder.lockCanvas();
                 canvas.drawRGB(255, 0, 0);
-                Rect myCar = new Rect();
-                Rect enemyCar = new Rect();
+                Rect myCarRect = new Rect();
+                Rect enemyCarRect = new Rect();
                 canvas.drawBitmap(bg,0,0,null);
-                canvas.drawText("Blah", 0, 0, null);
                 canvas.drawBitmap(car, carPos, 1550, null);
+
+                canvas.drawBitmap(enemyCar, enemyCarX, enemyCarPos, null);
                 holder.unlockCanvasAndPost(canvas);
 
             }
